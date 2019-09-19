f98ed4d83349b8d433cb37fc01f668eeeaf6fdfe
zahidcserana <zahid1004048@gmail.com>
2019-09-19 18:21:54 +0600

    dashboard

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardModel } from '../models/dashboard.model';
+import { StockChart } from 'angular-highcharts';
+import { HomeService } from '../services/home.service';
+import { ScriptLoaderService } from 'src/app/common/script-loader.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +11,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  summary: DashboardModel = new DashboardModel();
+  order: StockChart;
+  sale: StockChart;
+  statistics;
+  orderData = [];
+  saleData = [];
+  constructor(
+    private homeService: HomeService,
+    private _script: ScriptLoaderService,
+    private route: ActivatedRoute,
+  ) {
   }
 
+  ngOnInit() {
+    this.getSummary();
+    this.statistics = this.route.data.subscribe(
+      val => {
+        this.statistics = val && val['dashboard'] ? val['dashboard'] : [];
+        this.orderData = this.statistics.order;
+        this.saleData = this.statistics.sale;
+        console.log(this.statistics);
+      }
+    );
+    this.getOrderChart();
+    this.getSaleChart();
+  }
+  getSummary() {
+    this.homeService.getSummary()
+      .subscribe((res) => {
+        this.summary = res;
+      });
+  }
+  getOrderChart() {
+    this.order = new StockChart({
+      title: {
+        // text: 'Purchase Chart'
+      },
+      rangeSelector: {
+        selected: 0,
+        inputEnabled: false
+      },
+      series: [{
+        tooltip: {
+          valueDecimals: 2
+        },
+        name: 'Purchase',
+        data: this.orderData,
+        type: undefined
+      }]
+    });
+  }
+
+  getSaleChart() {
+    this.sale = new StockChart({
+      title: {
+        // text: 'Sale Chart'
+      },
+      rangeSelector: {
+        selected: 0,
+        inputEnabled: false
+      },
+      series: [{
+        tooltip: {
+          valueDecimals: 2
+        },
+        name: 'Sale',
+        data: this.saleData,
+        type: undefined
+      }]
+    });
+  }
 }
--- src/app/home/dashboard/dashboard.module.ts
+++ src/app/home/dashboard/dashboard.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-
-
+import { HomeService } from '../services/home.service';
+import stock from 'highcharts/modules/stock.src';
+import more from 'highcharts/highcharts-more.src';
+import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
+export function highchartsModules() {
+  // apply Highcharts Modules to this array
+  return [stock, more];
+}
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
-  ]
+    DashboardRoutingModule,
+    ChartModule
+  ],
+  providers: [HomeService, { provide: HIGHCHARTS_MODULES, useFactory: highchartsModules }]
 })
 export class DashboardModule { }
