f31a62efbd6c94c353e2c6ad026d6967528c5011
zahidcserana <zahid1004048@gmail.com>
2019-09-25 18:50:02 +0600

    acl user

--- src/app/home/includes/header/header.component.ts
+++ src/app/home/includes/header/header.component.ts
@@ -1,33 +1,23 @@
-import { AuthService } from 'src/app/auth/auth.service';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { AuthService } from "src/app/auth/auth.service";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
-  user;
-  constructor(
-    private router: Router,
-    private authService: AuthService
-  ) { }
+  constructor(private router: Router, private authService: AuthService) {}
 
-  ngOnInit() {
-    this.user = this.authService.getUser();
-    console.log('user');
-    console.log(this.user.user_type);
-  }
-  getAcl() {
-    // let aclList = {
-    //   'ADMIN' : ['dashboard', 'sale', 'purchase', 'user', 'inventory', 'report']
-    // };
-    return false;
+  ngOnInit() {}
+  checkAcl(page) {
+    return this.authService.checkAcl(page);
   }
   logout() {
     localStorage.removeItem("currentUser");
     sessionStorage.removeItem("currentUser");
     this.router.navigate(["/login"]);
+    window.location.reload();
   }
 }
